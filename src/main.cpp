21764f38c09f5c79afede509f6b0857c3caf7195
Baisayan <bbbbaisayan@gmail.com>
2026-04-03 20:53:50 +0000

    support key with expiry

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,15 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <poll.h>
+#include <chrono>
 
-std::unordered_map<std::string, std::string> g_kv_store;
+struct ValueEntry {
+    std::string value;
+    std::chrono::time_point<std::chrono::steady_clock> expiry_time;
+    bool has_expiry = false;
+};
+
+std::unordered_map<std::string, ValueEntry> g_kv_store;
 
 std::vector<std::string> split_resp(const std::string& s) {
     std::vector<std::string> parts;
@@ -45,15 +52,39 @@ void handle_client(int client_fd) {
         send(client_fd, resp.c_str(), resp.length(), 0);
     }
     else if (command == "SET") {
-        g_kv_store[parts[4]] = parts[6];
+        std::string key = parts[4];
+        std::string value = parts[6];
+        ValueEntry entry;
+        entry.value = value;
+
+        if (parts.size() >= 10) {
+            std::string option = parts[8];
+            for (auto &c : option) c = toupper(c);
+            
+            if (option == "PX") {
+                long long ms = std::stoll(parts[10]);
+                entry.has_expiry = true;
+                entry.expiry_time = std::chrono::steady_clock::now() + std::chrono::milliseconds(ms);
+            }
+        }
+
+        g_kv_store[key] = entry;
         send(client_fd, "+OK\r\n", 5, 0);
     }
     else if (command == "GET") {
         std::string key = parts[4];
+
         if (g_kv_store.count(key)) {
-            std::string val = g_kv_store[key];
-            std::string resp = "$" + std::to_string(val.length()) + "\r\n" + val + "\r\n";
-            send(client_fd, resp.c_str(), resp.length(), 0);
+            ValueEntry &entry = g_kv_store[key];
+
+            // expiry check
+            if (entry.has_expiry && std::chrono::steady_clock::now() >= entry.expiry_time) {
+                g_kv_store.erase(key);
+                send(client_fd, "$-1\r\n", 5, 0);
+            } else {
+                std::string resp = "$" + std::to_string(entry.value.length()) + "\r\n" + entry.value + "\r\n";
+                send(client_fd, resp.c_str(), resp.length(), 0);
+            }
         } else {
             send(client_fd, "$-1\r\n", 5, 0); // Null Bulk String
         }
