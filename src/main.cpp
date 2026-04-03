f681fe263579e96a5d4cb384e7bc8a7e52f9cfec
Baisayan <bbbbaisayan@gmail.com>
2026-04-03 18:17:53 +0000

    listen to multiple pings from multiple clients

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,90 @@
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <cstring>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
 #include <netdb.h>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <thread>
+#include <unistd.h>
+
+void handle_client(int client_fd) {
+
+  char buffer[1024] = {0};
+  const char *response = "+PONG\r\n";
+  // recieve loop
+  while (true) {
+    int ret = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+
+    if (ret <= 0) {
+      std::cout << "Client disconnected\n";
+      memset(
+          buffer, 0,
+          sizeof(
+              buffer)); // clear buffer for the next client to get more messages
+      break;
+    }
+
+    send(client_fd, response, strlen(response), 0);
+  }
+
+  close(client_fd);
+}
 
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  
+
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
+    std::cerr << "Failed to create server socket\n";
+    return 1;
   }
-  
+
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) <
+      0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(6379);
-  
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) !=
+      0) {
     std::cerr << "Failed to bind to port 6379\n";
     return 1;
   }
-  
+
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
     std::cerr << "listen failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
   std::cout << "Waiting for a client to connect...\n";
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  // You can use print statements as follows for debugging, they'll be visible
+  // when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
-  // Uncomment the code below to pass the first stage
-  
-  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  std::cout << "Client connected\n";
-  
-  while (1) {
-    char buffer[1024] = {0};
-    read(client_fd, buffer, sizeof(buffer));
-    const char *response = "+PONG\r\n";
-    send(client_fd, response, strlen(response), 0);
-  }
-  
-  close(server_fd);
+  while (true) {
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+                           (socklen_t *)&client_addr_len);
 
+    std::cout << "Client connected\n";
+    std::thread client_thread(handle_client, client_fd);
+    client_thread.detach();
+  }
+  close(server_fd);
   return 0;
 }
